58d06ac27053b9f0c15bd5748501d7b193106d49
Hugo Berra <jtberra@gmail.com>
2023-04-26 17:35:29 -0600

    search engine change
    The Etree module that parsed the XML was replaced with lxml, a more efficient compilation module and action engine with large volumes of data.

--- cfdi_sorter.py
+++ cfdi_sorter.py
@@ -1,35 +1,29 @@
-#!/usr/bin/env python3.11
+#!/usr/bin/env python3.11.1
 # -*- coding: utf-8 -*-
 
 # script header
 # Author:           [Hugo Berra Salazar, ]
-# Creation date:    [04/07/2023]
+# Creation date:    [04/26/2023]
 # Description:      [Brief description of the purpose of the script]
 
 # import necessary modules
+from lxml import etree
 import os
 import shutil
-import xml.etree.ElementTree as ET
 
 # Define the subdirectories
-emisor_directory            = 'Emisor'
-receptor_directory          = 'Receptor'
-err_directory               = 'Errors'
+emisor_directory        = 'Emisor'
+receptor_directory      = 'Receptor'
+err_directory           = 'Error'
 
 # Define the sub-subdirectories
-ingresos_directory          = 'Ingresos'
-gastos_receptor_directory   = 'Gastos'
-gastos_emitidos_directory   = 'Gastos'
-ayudas_directory            = 'Ayudas'
-des_bon_dev_directory       = 'Descuento_Bonificaciones_Devoluciones'
-nomina_directory            = 'Nomina'
-pagosE_directory            = 'Pagos'
-pagosR_directory            = 'Pagos'
-
-# Define the ElementPath queries
-tipo_query                  = 'TipoDeComprobante'
-mpago_query                 = 'MetodoPago'
-version_query               = 'Version'
+ingresos_directory      = 'Ingresos'
+gastosR_directory       = 'Gastos'
+ayudas_directory        = 'Ayudas'
+des_bon_dev_directory   = 'Descuento_Bonificaciones_Devoluciones'
+nomina_directory        = 'Nomina'
+pagosE_directory        = 'Pagos'
+pagosR_directory        = 'Pagos'
 
 def get_cfdi_version(version):
     if   version == '4.0':
@@ -49,75 +43,73 @@ def filenames(directory):
                 yield os.path.join(root, file)
 
 def cfdi_sorter(rfc, directory):
+
     # Calls the function "filenames" Iterating over each XML file in the directory
     for filename in filenames(directory):
         try:
-            # Parse the XML file with ElementTree
-            tree        = ET.parse(filename)
-            root        = tree.getroot()
-
-            # Get the value of the "TipoDeComprobante" attribute
-            tipo        = root.get(tipo_query)
+            # Parse the XML file with lxml
+            tree = etree.parse(filename)
+            root = tree.getroot()
             
-            # Get the cfdi version
-            version     = root.get(version_query)
+            if root.tag.endswith('Comprobante'):
+                tipo        = root.get('TipoDeComprobante')
+                metodo_pago = root.get('MetodoPago')
+                version     = root.get('Version')
+
             emisor_query, receptor_query = get_cfdi_version(version)
-
-            # Get the "Emisor" element
-            emisor = root.find(emisor_query)
-
-            # Get the "Receptor" element
-            receptor = root.find(receptor_query)
-
-            # Get the value of the "MetodoPago" attribute
-            metodo_pago = root.get(mpago_query)
+            emisor      = root.find(f'.//{emisor_query}').get('Rfc')
+            receptor    = root.find(f'.//{receptor_query}').get('Rfc')
 
             # Check if emisor, receptor, tipo, and metodo_pago are not None
             if emisor is None or receptor is None or tipo is None:
-                print(f"{filename} : E1: does not meet the requirements of the CFDI standard.")
-                shutil.copy(filename, os.path.join(rfc, err_directory, os.path.basename(filename)))
-                continue
+                print(f"{filename} : E2: does not meet the requirements of the CFDI sorter standard.")
+                try:
+                    os.makedirs(os.path.join(rfc, err_directory, filename))
+                except FileExistsError :
+                    continue
+
 
             # Check if the value of metodo_pago is valid
             if metodo_pago not in ['PUE', 'PPD']:
                 if tipo == 'P':
-                    metodo_pago = 'PAGOS'
+                    metodo_pago = 'PAGO'
                 else:
-                    print(f"{filename} : E2: MetodoPago is not valid: {metodo_pago}")
+                    print(f"{filename} : E3: MetodoPago is not valid: {metodo_pago}")
                     continue
-            
+
             # Create the appropriate sub-subdirectory and copy the XML file based on the attribute values
-            if   tipo == 'I' and emisor.get('Rfc')      == rfc:
+            if   tipo == 'I' and emisor      == rfc:
                 subdirectory = ingresos_directory
-            elif tipo == 'I' and receptor.get('Rfc')    == rfc:
-                subdirectory = gastos_receptor_directory
-            elif tipo == 'I' and emisor.get('Rfc')      == rfc:
-                subdirectory = gastos_emitidos_directory
-            elif tipo == 'E' and emisor.get('Rfc')      == rfc:
+            elif tipo == 'I' and receptor    == rfc:
+                subdirectory = gastosR_directory
+            elif tipo == 'E' and emisor      == rfc:
                 subdirectory = ayudas_directory
-            elif tipo == 'E' and receptor.get('Rfc')    == rfc:
+            elif tipo == 'E' and receptor    == rfc:
                 subdirectory = des_bon_dev_directory
-            elif tipo == 'N' and emisor.get('Rfc')      == rfc:
+            elif tipo == 'N' and emisor      == rfc:
                 subdirectory = nomina_directory
-            elif tipo == 'P' and emisor.get('Rfc')      == rfc:
+            elif tipo == 'P' and emisor      == rfc:
                 subdirectory = pagosE_directory
-            elif tipo == 'P' and receptor.get('Rfc')    == rfc:
+            elif tipo == 'P' and receptor    == rfc:
                 subdirectory = pagosR_directory
             else:
-                print(f"{filename} : E3: does not meet the requirements of the CFDI standard. Or Wrong RFC")
-                continue
+                print(f"E3: {filename} does not belong to any of the classifier folders.")
+                try:
+                    os.makedirs(os.path.join(rfc, err_directory, filename))
+                except FileExistsError :
+                    continue
 
             # Create the sub-subdirectory inside the appropriate subdirectory
-            sub_subdirectory = os.path.join(emisor_directory, subdirectory, metodo_pago) if emisor.get('Rfc') == rfc else os.path.join(receptor_directory, subdirectory, metodo_pago)
+            sub_subdirectory = os.path.join(emisor_directory, subdirectory, metodo_pago) if emisor == rfc else os.path.join(receptor_directory, subdirectory, metodo_pago)
             try:
                 os.makedirs(os.path.join(rfc, sub_subdirectory))
-            except FileExistsError:
+            except FileExistsError :
                 pass
 
             # Copy the XML file to the appropriate sub-subdirectory
             shutil.copy(filename, os.path.join(rfc, sub_subdirectory, os.path.basename(filename)))
 
-        except ET.ParseError:
-            print(f"{filename} could not be parsed.")
+        except etree.ParseError:
+            print(f"E1: {filename} could not be parsed.")
         except Exception as e:
-            print(f"{filename} could not be processed due to an error: {e}.")
+            print(f"E1: {filename} could not be processed due to an error: {e}.")
